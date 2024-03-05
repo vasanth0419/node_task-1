3867077e04fcf8840cf4bcb278195d61cfdcb0f6
vasanth0419 <vasanthsalomonv1904@gmail.com>
2024-03-05 22:53:58 +0530

    .

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ const time = express();
 
 const PORT = 4000;
 
-time.get("/write", (req, res) => {
-  const current_date_time = format(new Date(), "dd-MM-yyyy-HH-mm-ss");
-  const filePath = `current_Time_stamp/${current_date_time}.txt`;
+// time.get("/write", (req, res) => {
+//   const current_date_time = format(new Date(), "dd-MM-yyyy-HH-mm-ss");
+//   const filePath = `current_Time_stamp/${current_date_time}.txt`;
 
-  fs.writeFileSync(filePath, `${current_date_time}`, "utf8");
-  res.status(200).json({ message: "Timestamp written successfully" });
-});
+//   fs.writeFileSync(filePath, `${current_date_time}`, "utf8");
+//   res.status(200).json({ message: "Timestamp written successfully" });
+// });
 
 time.get("/read", (req, res) => {
   const current_date_time = format(new Date(), "dd-MM-yyyy-HH-mm-ss");
