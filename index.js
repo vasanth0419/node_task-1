02f73320bd4ec2a02b5aba1a0d5a5e33defe91dd
vasanth0419 <vasanthsalomonv1904@gmail.com>
2024-03-12 17:44:58 +0530

    update

--- index.js
+++ index.js
@@ -14,13 +14,24 @@ time.get("/write", (req, res) => {
   res.status(200).json({ message: "Timestamp written successfully" });
 });
 
-time.get("/read", (req, res) => {
-  const current_date_time = format(new Date(), "dd-MM-yyyy-HH-mm-ss");
-  const filePath = `current_Time_stamp/${current_date_time}.txt`;
-  fs.writeFileSync(filePath, `${current_date_time}`, "utf8");
-  let data = fs.readFileSync(filePath, "utf8");
-  res.status(200).send(data);
+time.get("/getTextFiles", (req, res) => {
+  const filepath = "current_Time_stamp";
+  fs.readdir(filepath, (err, files) => {
+    if (err) {
+      console.log(err);
+      res
+        .status(500)
+        .send("An error occurred while listing the files from directory");
+    } else {
+      const textFiles = files.filter((file) => path.extname(file) === ".txt");
+      res.status(200).json(textFiles);
+    }
+  });
 });
+
+
+
+
 
 time.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
